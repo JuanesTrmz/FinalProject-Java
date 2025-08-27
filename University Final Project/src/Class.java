efdee43971af79a4650a6b5645e4ac9c5c0a37b4
Juan Esteban Trillos Monterroza <juan.trillos@globant.com>
2025-08-27 15:30:46 -0500

    First try of Class code (?

--- University Final Project/src/Class.java
+++ University Final Project/src/Class.java
@@ -1,2 +1,47 @@
+import java.util.List;
+
 public class Class {
+    private String name;
+    private int classroom;
+    protected List<Student> studentsList;
+    protected Teacher teacher;
+
+    public Class(String name, int classroom, List<Student> studentsList, Teacher teacher) {
+        this.name = name;
+        this.classroom = classroom;
+        this.studentsList = studentsList;
+        this.teacher = teacher;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(int classroom) {
+        this.classroom = classroom;
+    }
+
+    public List<Student> getStudentsList() {
+        return studentsList;
+    }
+
+    public void setStudentsList(List<Student> studentsList) {
+        this.studentsList = studentsList;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
 }
